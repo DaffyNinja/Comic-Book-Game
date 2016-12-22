a87be375e1a33fec25473f44410cf40c3aa95a3e
DaffyNinja <marcusday117@gmail.com>
2016-12-22 11:55:47 +1100

    Placed in new sprites for running and guns
    Made Run and Gun script
    
    placed run and gun in prototype scene
    
    Made more borders

---
+++ ComicBookGame/Assets/Scripts/Run.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Run : MonoBehaviour
+{
+
+    Animator animate;
+
+    public float runTime;
+    float timer;
+
+    bool pressed;
+
+    // Use this for initialization
+    void Start()
+    {
+        animate = GetComponent<Animator>();
+
+        animate.enabled = false;
+
+        timer = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pressed)
+        {
+            animate.enabled = true;
+
+            timer += Time.deltaTime;
+
+            if (timer >= runTime)
+            {
+                print("Done");
+            }
+        }
+        else
+        {
+            animate.enabled = false;
+
+            timer = 0;
+        }
+
+    }
+
+    private void OnMouseDown()
+    {
+        pressed = true;
+    }
+
+    private void OnMouseUp()
+    {
+        pressed = false;
+    }
+
+}
